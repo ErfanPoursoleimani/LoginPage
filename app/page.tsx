abbc8d07bf824bae8d8b057139fb6604b8cc1648
Erfan Poursoleimani <erfanpoursoleimani@gmail.com>
2025-04-29 08:26:38 +0330

    Sync

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,84 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react'
+import { z } from 'zod'
+import { userSchema } from './validationSchemas'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import { Status } from '@prisma/client'
+import { FaArrowRight } from 'react-icons/fa6'
+import axios from 'axios'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+type userData = z.infer<typeof userSchema>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface Props {
+  users: {phoneNumber: string, status: Status}[],
+  setSignInDisplay: Function,
 }
+
+const SignInPage = ({users, setSignInDisplay}: Props) => {
+
+    const router = useRouter()
+      
+    const {register, handleSubmit, formState: { errors, isSubmitted }} = useForm<userData>({
+      resolver: zodResolver(userSchema)
+    })
+  
+    const [error, setError] = useState('')
+
+    const onSubmit = handleSubmit( async(data) => {
+      try {
+        let t = 0
+        for (let i = 0; i < users.length; i++) {
+          users[i].phoneNumber === data.phoneNumber ? t++ : null
+        }
+        console.log(data, t)
+        if(t !== 0) {
+          setSignInDisplay('none')
+          router.push('/' + data.phoneNumber)
+        } else {
+          await axios.post('/api/users', data)
+          setSignInDisplay('none')
+          router.push('/' + data.phoneNumber)
+        }
+        router.refresh()
+      } catch (error) {
+        setError('An unexpected error occurred.')
+      }
+    })
+
+    const handleDisplay = () => {
+      setSignInDisplay('none')
+    }
+  return (
+    <div className='hamburger-nav-animation overflow-scroll fixed top-[80px] p-12 z-100 w-[100vw] h-[calc(100vh-80px)] border-t-1 border-[#000] backdrop-blur-3xl bg-[#00000081] flex flex-col items-center justify-center'>
+      <div className="mb-15 cursor-pointer text-xl bg-[var(--foreground)] p-2 rounded-full self-end" onClick={handleDisplay}>
+        <FaArrowRight />
+      </div>
+      <Box className="bg-white rounded-2xl flex flex-col items-center gap-5" p={3}
+      component="form"
+      sx={{ '& .MuiTextField-root': {width: '25ch'}}}
+      noValidate
+      autoComplete="off"
+      id='form'
+      onSubmit={onSubmit}
+      >
+        <div>
+          <TextField
+            id="outlined-disabled"
+            label="شماره تلفن همراه"
+            type='number'
+            {...register("phoneNumber")}
+          />
+          <p className='text-red-600 text-[12px] mt-2'>{errors.phoneNumber?.message}</p>
+        </div>
+        {error && <p className='text-red-600'>{error}</p>}
+        <button className='w-full h-15 bg-black rounded-[10px] hover:scale-101 transition-all' disabled={isSubmitted}>ارسال کد تایید</button>
+      </Box>
+    </div>
+  )
+}
+
+export default SignInPage
